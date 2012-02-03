f97d5863f3f66cd85152e1a231418eaf7aa00426
Tetsuya MORI <monry84@gmail.com>
2012-02-03 14:17:45 +0900

    String to String の際に g オプションを付ける

--- replaceAttr/replaceAttr.js
+++ replaceAttr/replaceAttr.js
@@ -10,33 +10,37 @@ void function($) {
     this.each(
       function(index, element) {
         var $element = $(element);
-        if ((typeof replacement == 'string') && /#\{?\d+\}?/.test(replacement)) {
-          var replacement_list = [];
-          var RE = new RegExp(/#\{(\d+)\}|#(\d+)/g);
-          var matches;
-          while ((matches = RE.exec(replacement)) != null) {
-            var index = matches[1] || matches[2];
-            replacement_list[index] = matches[0];
-          }
-          $element.attr(
-            attribute_name
-            , $element.attr(attribute_name).replace(
-              pattern
-              , function() {
-                var argument_list = arguments;
-                $.each(
-                  replacement_list
-                  , function(index, value) {
-                    if (typeof value == 'undefined') {
-                      return;
+        if ((typeof replacement == 'string')) {
+          if (/#\{?\d+\}?/.test(replacement)) {
+            var replacement_list = [];
+            var RE = new RegExp(/#\{(\d+)\}|#(\d+)/g);
+            var matches;
+            while ((matches = RE.exec(replacement)) != null) {
+              var index = matches[1] || matches[2];
+              replacement_list[index] = matches[0];
+            }
+            $element.attr(
+              attribute_name
+              , $element.attr(attribute_name).replace(
+                pattern
+                , function() {
+                  var argument_list = arguments;
+                  $.each(
+                    replacement_list
+                    , function(index, value) {
+                      if (typeof value == 'undefined') {
+                        return;
+                      }
+                      replacement = replacement.replace(replacement_list[index], argument_list[index]);
                     }
-                    replacement = replacement.replace(replacement_list[index], argument_list[index]);
-                  }
-                );
-                return replacement;
-              }
-            )
-          );
+                  );
+                  return replacement;
+                }
+              )
+            );
+          } else {
+            $element.attr(attribute_name, $element.attr(attribute_name).replace(new RegExp(pattern, 'g'), replacement));
+          }
         } else {
           $element.attr(attribute_name, $element.attr(attribute_name).replace(pattern, replacement));
         }
