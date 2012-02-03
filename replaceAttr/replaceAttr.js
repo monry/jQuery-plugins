78f1f0387c13422270b8f7ee1a0ff469c2aa5d40
Tetsuya MORI <monry84@gmail.com>
2012-02-03 12:03:55 +0900

    正規表現マッチングに於ける後方参照を実装
    ついでに命名規約変更

---
+++ replaceAttr/replaceAttr.js
@@ -0,0 +1,48 @@
+/*
+ * jQuery.replaceAttr - jQuery Plugin
+ *
+ * Version: 1.0.1
+ * Revision: $Rev$
+ * Date: $Date$
+ */
+void function($) {
+  $.fn.replaceAttr = function(attribute_name, pattern, replacement) {
+    this.each(
+      function(index, element) {
+        var $element = $(element);
+        if ((typeof replacement == 'string') && /#\{?\d+\}?/.test(replacement)) {
+          var replacement_list = [];
+          var RE = new RegExp(/#\{(\d+)\}|#(\d+)/g);
+          var matches;
+          while ((matches = RE.exec(replacement)) != null) {
+            var index = matches[1] || matches[2];
+            replacement_list[index] = matches[0];
+          }
+          $element.attr(
+            attribute_name
+            , $element.attr(attribute_name).replace(
+              pattern
+              , function() {
+                var argument_list = arguments;
+                $.each(
+                  replacement_list
+                  , function(index, value) {
+                    if (typeof value == 'undefined') {
+                      return;
+                    }
+                    replacement = replacement.replace(replacement_list[index], argument_list[index]);
+                  }
+                );
+                return replacement;
+              }
+            )
+          );
+        } else {
+          $element.attr(attribute_name, $element.attr(attribute_name).replace(pattern, replacement));
+        }
+      }
+    );
+    return this;
+  };
+}(jQuery);
+
